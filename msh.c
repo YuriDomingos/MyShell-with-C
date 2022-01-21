e113986505443bf2853a75cc8db8a62e521338ea
YuriDomingos <josemarlabareda@gmail.com>
2022-01-21 11:58:08 +0100

    subindo o codigo

--- msh.c
+++ msh.c
@@ -9,6 +9,7 @@ Objectivo : Implementar o projecto de S.O 2
 #include<string.h>
 #include<unistd.h>
 #define TAMANHO_COMANDO 100
+#define EXECUTANDO 1
 
 
  void apresentar_interface()
@@ -60,6 +61,19 @@ void verificar_comando_history( char comando_introduzido[])
 
 void apagar_informacoes_do_comando_history(char comando_introduzido[])
 {
+    if ( strcmp(comando_introduzido,"clear") == 0)
+    {
+        // vamos ler o comando que apaga dados no linux
+         char *caminho_do_comando_rm  = "bin/rm";
+         char *comando = "rm";
+         char *caminho_do_ficheiro_historico = "/home/yuri/.bash_history";
+         execlp(caminho_do_comando_rm,comando, caminho_do_ficheiro_historico);
+
+    }
+    else
+    {
+        printf("Erro, comando %s nao reconhecido ",comando_introduzido);
+    }
     
 }
 
@@ -71,10 +85,15 @@ int main (void )
 
     // Chamada de função 
 
-    apresentar_interface();
-    ler_comando_introduzido(comandos);
-    espaco();
-    verificar_comando_history(comandos);
+    while(EXECUTANDO)
+    {
+       apresentar_interface();
+       ler_comando_introduzido(comandos);
+       espaco();
+       verificar_comando_history(comandos);
+       apagar_informacoes_do_comando_history(comandos);
+    }
+  
    
     return 0;
 }
